0df16e85f97de08316e57e1a55893955f474e79c
DorisBian <bianziying520@gmail.com>
2018-09-27 14:42:01 +0800

    逻辑运算符

---
+++ pythonPractice/LogicalOperators.py
@@ -0,0 +1,18 @@
+home=1
+car=3
+lover=1
+
+if home>=2 and car>=3:
+    print("可以结婚")
+
+else:
+    print("不能结婚")
+
+if (home>=2 and car>=3) or lover==1:
+    print("可以结婚")
+
+else:
+    print("不能结婚")
+
+
+
