3d86a0f317c668d47c6e68954cd81a1708de1170
DorisBian <bianziying520@gmail.com>
2018-10-09 14:52:02 +0800

    secondParagram

---
+++ pythonPractice/test1/2.py
@@ -0,0 +1,22 @@
+
+age=11
+if age>=18:
+    print("可以上网")
+elif age>=17:
+    print("等两天就可以上网了")
+elif age>=12:
+    print("过两年才可以来")
+else:
+    print("走开")
+
+
+
+lover="亲爱的"
+print(lover)
+print(lover)
+print(lover)
+
+lover="宝贝儿"
+print(lover)
+print(lover)
+
