82f59a9eedd37ca5cec84d50f35b78a94b29d075
DorisBian <bianziying520@gmail.com>
2018-10-10 23:15:11 +0800

    公共函数

---
+++ pythonPractice/day7/PublicFunction.py
@@ -0,0 +1,24 @@
+#公共方法
+#合并：+
+print([1,2]+[3,4])
+#不支持字典+字典
+#print({"name":"hh","age":19}+{"name":"nn","age":23})
+print("hdgfh"+"bvhjs bmng")
+print((11,22,33)+("jsk","674658",19))
+
+#复制：*
+print("hello_"*4)
+print((11,33)*3)
+print([8,2,"xx"]*4)
+
+#查看元素是否存在：in
+print(3 in [1,2])
+print(3 in (1,2,3,4))
+print("h" in "hsvkjfvfs")
+
+print("\t")
+#查看元素是否不存在：not in
+print(3 not in [1,2])
+print(3 not in (1,2,3,4))
+print("h"not in "hsvkjfvfs")
+
