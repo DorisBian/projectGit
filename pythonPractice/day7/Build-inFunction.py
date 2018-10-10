418e4109a06d0f2935ce849e25d1109bacf8d03b
DorisBian <bianziying520@gmail.com>
2018-10-10 23:07:51 +0800

    内置函数

---
+++ pythonPractice/day7/Build-inFunction.py
@@ -0,0 +1,16 @@
+#内置函数
+print(max("adbhcdXF"))
+print(min("adbhcdXF"))
+print(max([11,22,54099,346537,236]))
+print(min([11,22,54099,346537,236]))
+print(max((1,45,8,95,216)))
+print(min((1,45,8,95,216)))
+#删除
+a={"name":"hh","age":19}
+del(a["name"])
+print(a)
+
+print(len("bvskvvavfdf"))
+
+#比较,cmp函数是Python2的，Python3实现比较需要使用对象，较复杂
+#print(cmp("hello","hjvkavfiyw"))
