eec6ea8876997c5710318794c36d06d03b51b608
DorisBian <bianziying520@gmail.com>
2018-12-20 20:09:07 +0800

    生成器3

---
+++ pythonPractice/AdvancedFeatures/Builder3.py
@@ -0,0 +1,13 @@
+# 生成器
+# send
+def test():
+    i=0
+    while i<5:
+        temp=yield i
+        print(temp)
+        i+=1
+
+t=test()
+print(t.__next__())
+print(t.__next__())
+print(t.send("hhhhh"))
