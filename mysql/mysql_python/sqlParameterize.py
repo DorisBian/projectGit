c5e1d35f67e796826a2cf1e1f2f64c04271a8ce1
DorisBian <bianziying520@gmail.com>
2019-06-22 15:11:05 +0800

    sql语句参数化

---
+++ mysql/mysql_python/sqlParameterize.py
@@ -0,0 +1,21 @@
+import pymysql
+
+
+try:
+    # 打开
+    conn=pymysql.connect(host='localhost',port=3306,user='root',password='Root.123',database='python3',charset='utf8')
+    cursor1=conn.cursor()
+
+    name=input("请输入学生姓名:")
+    sql='insert into students(name) values(%s)'
+    cursor1.execute(sql,[name])    # 第二个参数是个列表
+    # 提交事务
+    conn.commit()
+
+    # 关闭
+    cursor1.close()
+    conn.close()
+    print('ok')
+
+except Exception as result:
+    print(result)
